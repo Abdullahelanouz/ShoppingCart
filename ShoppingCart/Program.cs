74155e2d49af425aa4d96ed62491456629976dbb
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-08-29 19:15:40 +0300

    SeedData

--- ShoppingCart/Program.cs
+++ ShoppingCart/Program.cs
@@ -27,5 +27,8 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
+SeedData.SeedDatabase(context);
+
 
 app.Run();

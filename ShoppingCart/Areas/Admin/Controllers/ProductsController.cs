76da905c7b0d5d5ca56da8403a4e12ee054a2c21
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-09-02 13:42:07 +0300

    Delete Product

--- ShoppingCart/Areas/Admin/Controllers/ProductsController.cs
+++ ShoppingCart/Areas/Admin/Controllers/ProductsController.cs
@@ -135,26 +135,26 @@ namespace ShoppingCart.Areas.Admin.Controllers
             return View(product);
         }
 
-        //public async Task<IActionResult> Delete(long id)
-        //{
-        //    Product product = await _context.Products.FindAsync(id);
+        public async Task<IActionResult> Delete(long id)
+        {
+            Product product = await _context.Products.FindAsync(id);
 
-        //    if (!string.Equals(product.Image, "noimage.png"))
-        //    {
-        //        string uploadsDir = Path.Combine(_webHostEnvironment.WebRootPath, "media/products");
-        //        string oldImagePath = Path.Combine(uploadsDir, product.Image);
-        //        if (System.IO.File.Exists(oldImagePath))
-        //        {
-        //            System.IO.File.Delete(oldImagePath);
-        //        }
-        //    }
+            if (!string.Equals(product.Image, "noimage.png"))
+            {
+                string uploadsDir = Path.Combine(_webHostEnvironment.WebRootPath, "media/products");
+                string oldImagePath = Path.Combine(uploadsDir, product.Image);
+                if (System.IO.File.Exists(oldImagePath))
+                {
+                    System.IO.File.Delete(oldImagePath);
+                }
+            }
 
-        //    _context.Products.Remove(product);
-        //    await _context.SaveChangesAsync();
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
 
-        //    TempData["Success"] = "The product has been deleted!";
+            TempData["Success"] = "The product has been deleted!";
 
-        //    return RedirectToAction("Index");
-        //}
+            return RedirectToAction("Index");
+        }
     }
 }
